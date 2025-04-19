6b4b492b44f2005afbe5d6bdaf7fc070f39372b1
van.tran <van.tran@modec.com>
2025-04-20 06:58:25 +0700

    try adding SQL injection

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from pages import views
 
+from .api_views import api_home  # Import the api_home function
+
 urlpatterns = [
+    path("api/", api_home, name="api_home"),  # Map the API endpoint
     path("", views.home, name="home"),
 ]
